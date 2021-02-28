ba0618225b46f34c865db45bd4dcf9a9a701f5a7
dmentry <slipin2020@mail.ru>
2021-02-28 10:59:29 +0300

    Add games#show

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,5 +1,67 @@
 class Game < ApplicationRecord
+  TOTAL_ERRORS_ALLOWED = 7
+
   belongs_to :word
 
   scope :sorted, -> { order(created_at: :desc) }
+
+  def errors_letters
+    guesses - normalized_letters
+  end
+
+  def errors_made
+    errors_letters.length
+  end
+
+  def errors_allowed
+    TOTAL_ERRORS_ALLOWED - errors_made
+  end
+
+  def letters_to_guess
+    result = letters.map do |letter|
+      if guesses.include?(normalize_letters(letter))
+        letter
+      else
+        nil
+      end
+    end
+    return result
+  end
+
+  def over?
+    return won? || lost?
+  end
+
+  def play!(letter)
+    letter = normalize_letters(letter)
+    if !over? && !guesses.include?(letter)
+      guesses << letter
+    end
+  end
+
+  def won?
+    return (normalized_letters - guesses).empty?
+  end
+
+  def lost?
+    return errors_allowed == 0
+  end
+
+  def normalize_letters(letter)
+    if letter == 'Ё'
+      return 'Е'
+    elsif letter == 'Й'
+      return 'И'
+    else
+      return letter
+    end
+  end
+
+  def normalized_letters
+    letters.map { |letter| normalize_letters(letter) }
+  end
+
+  def letters
+    word.letters
+  end
 end
